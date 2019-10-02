dd0135b0c6e548e2ebb3cde3b8fa0e6fbdadad2d
JP Given <jgiven@gmail.com>
2019-10-01 18:21:52 -0700

    Dedupe issue with yarn.lock file and NPM publishing. Bump.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+// https://github.com/Polymer/polymer/issues/5407#issuecomment-437448950
+// https://www.npmjs.com/package/@lrnwebcomponents/deduping-fix
+import '@lrnwebcomponents/deduping-fix';
 import './components/moxi-left-nav';
