f50e1662e5d2e3a61737bf738478b2db9f39fab1
JP Given <jgiven@gmail.com>
2019-10-01 17:39:22 -0700

    ...

--- src/components/moxi-left-nav.js
+++ src/components/moxi-left-nav.js
@@ -3,12 +3,17 @@ import { LitElement, html, css } from 'lit-element';
 class MoxiLeftNav extends LitElement {
   
   static get properties() {
-    return { message: { type: String } };
+    return { 
+      message: { type: String },
+    };
   }
 
   static get styles() {
     return css`
       #moxi-left-navigation {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
         font-family: 'Muli', sans-serif;
         position: absolute;
         top: 0;
@@ -16,6 +21,8 @@ class MoxiLeftNav extends LitElement {
         bottom: 0;
         width: 225px;
         background: #000;
+        padding: 0;
+        overflow-y: auto;
       }
       #brokerage-logo {
         padding: 16px;
@@ -23,6 +30,19 @@ class MoxiLeftNav extends LitElement {
       #brokerage-logo img {
         display:block;
         margin: auto;
+      }
+      #moxi-left-nav-search {
+        margin-bottom: 16px;
+        padding: 0 16px;
+      }
+      #moxi-left-nav-search input {
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        width: 193px;
+        padding: 5.5px;
+        border-radius: 25px;
+        border: 0;
       }
     `
   }
@@ -36,6 +56,9 @@ class MoxiLeftNav extends LitElement {
       <div id="moxi-left-navigation">
         <div id="brokerage-logo">
           <img src="https://www.windermere.com/assets/shared/WRE_logo_white-268x100-18c23149838895d8ecfa41867b9bb393.png" width="150" />
+        </div>
+        <div id="moxi-left-nav-search">
+          <input name="moxi-search" placeholder="Search Anything">
         </div>
       </div>
     `;
